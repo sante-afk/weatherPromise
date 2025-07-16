2eb1b9b480cf02407d0539c847dc9e4d3774ac04
sante <86688897+sshyta@users.noreply.github.com>
2025-07-16 23:25:21 +0300

    add: func getWeatherDescription

--- weather.js
+++ weather.js
@@ -6,14 +6,15 @@
 
 (() => {
     _getCoordLonLat();
+    _getWeatherDescription();
 })();
 
 function _getCoordLonLat () {
     return new Promise ((resolve, reject) => {
         fetch("https://api.openweathermap.org/data/2.5/weather?" +
-            "lat=&" +
-            "lon=&" +
-             "appid=")
+            "lat=57.62987&" +
+            "lon=39.87368&" +
+             "appid=bd5e378503939ddaee76f12ad7a97608")
             .then(response => {
                 return response.json();
             })
@@ -39,5 +40,33 @@ function _getCoordLonLat () {
                 }
             })
                 
+    });
+}
+
+function _getWeatherDescription () {
+    return new Promise ((resolve, reject) => {
+        fetch("https://api.openweathermap.org/data/2.5/weather?lat=57.62987&lon=39.87368&appid=bd5e378503939ddaee76f12ad7a97608")
+            .then(response => {
+                return response.json();
+            })
+                .then(oData => {
+                    if (oData) {
+                        const weatherDescription = document.getElementById('weatherDescription');
+                        weatherDescription.textContent = oData.weather[0].description;
+                        resolve(oData);
+                    }
+                })
+                .catch(oError => {
+                    if (oError) {
+                        new Error('Looks like bad weather(' + oError.status);
+                        reject(oError);
+                    }
+                })
+            .catch(oError => {
+                if (oError) {
+                    new Error('response error' + oError.status);
+                    reject(oError);
+                }
+            })
     });
 }
